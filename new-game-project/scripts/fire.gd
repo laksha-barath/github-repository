f6d02bffdca4aec14c4d83b60472798a6bc2d995
laksha-barath <lbarathkumar@students.mags.school.nz>
2026-06-16 09:39:41 +1200

    gem display

--- new-game-project/scripts/fire.gd
+++ new-game-project/scripts/fire.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
 
+
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var fire_gem_counter = 0
 
+@onready var coin_label = %Label2
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +14,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("p2_jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("p2_left", "p2_right")
+	var direction := Input.get_axis("p1_left", "p1_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("fire_gem"):
+		fire_gem_counter += 1
+		coin_label.text = str(fire_gem_counter)
--- new-game-project/scripts/water.gd
+++ new-game-project/scripts/water.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var water_gem_counter = 0
+
+@onready var coin_label = %Label
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +13,37 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("p1_jump") and is_on_floor():
+	if Input.is_action_just_pressed("p2_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("p1_left", "p1_right")
+	var direction := Input.get_axis("p2_left", "p2_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("water_gem"):
+		water_gem_counter += 1
+		coin_label.text = str(water_gem_counter)
+	
+		
+		
+		
+		
+		#set_coin(water_gem_counter + 1)
+		#print(water_gem_counter)
+	
+# func set_coin(new_coin_count: int) -> void:
+	#water_gem_counter = new_coin_count
+	#coin_label.text = "water gem Count:" + str(water_gem_counter)
+
+
+
+	
+
+	
